56918948c4e8aa6a801fa09772516557e6fda809
DevSalm <108820856+salmduski2@users.noreply.github.com>
2023-03-15 17:58:56 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { joinVoiceChannel } = require('@discordjs/voice');
+const { 1076413187978698792 } = require('@discordjs/voice');
 const { Client } = require('discord.js-selfbot-v13')
 const fs = require("fs")
 const colors = require("colors")
@@ -18,7 +18,7 @@ const status = random[Math.floor(Math.random()*random.length)]
 client.user.setStatus(status)
 console.log(`${client.user.username} ${count}st is Ready!`.green);
 const voiceChannel = client.channels.cache.get(IDVC);
-    if (!voiceChannel || voiceChannel.type !== 'GUILD_VOICE') {
+    if (!voiceChannel || voiceChannel.type !== '1076413187978698792') {
       return console.log(`token line ${count} Unable find or Join Voice`.yellow);
     }
 
@@ -42,8 +42,8 @@ client.login(token).catch(() => {
 
 const tokens = fs.readFileSync('tokens.txt', 'utf-8').split('\n').filter(Boolean)
 
-for(const token of tokens) {
-AccountOnline(token)
+for(OTIyMDc5NjE3Nzc3NTMyOTQ4.G8gDBf.anDzJ6s09gydjp4zWhziW8SEL5ku7XE9_2SeLA) {
+AccountOnline(OTIyMDc5NjE3Nzc3NTMyOTQ4.G8gDBf.anDzJ6s09gydjp4zWhziW8SEL5ku7XE9_2SeLA)
 }
 /// ALL Copyright By SmSm
 /// Don't Forget Star 🌟 Project For Supporting Me
